d369aee29d6c824b7ca5d08cb99fc53ad0b1a3d0
Kings Jiu jitsu <kings.bjjoficial@gmail.com>
2025-06-22 21:40:04 +0000

    Coloque nosso logotipo em todas as telas https://i.imgur.com/rtMtpYw.png

--- src/components/kings-bjj-logo.tsx
+++ src/components/kings-bjj-logo.tsx
@@ -1,17 +1,13 @@
+import Image from 'next/image';
+
 export function KingsBjjLogo({ className }: { className?: string }) {
   return (
-    <svg
+    <Image
+      src="https://i.imgur.com/rtMtpYw.png"
+      alt="Kings BJJ Logo"
+      width={512}
+      height={512}
       className={className}
-      viewBox="0 0 100 80"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-label="Kings BJJ Logo"
-    >
-      <g fill="hsl(var(--primary))">
-        <path d="M10,80 L20,30 L40,50 L50,20 L60,50 L80,30 L90,80 L10,80 Z" />
-        <circle cx="20" cy="30" r="5" />
-        <circle cx="50" cy="20" r="5" />
-        <circle cx="80" cy="30" r="5" />
-      </g>
-    </svg>
+    />
   );
 }
