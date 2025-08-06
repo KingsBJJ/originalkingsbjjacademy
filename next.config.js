5549c5e44529422f532c74cd4628369bec4db633
Kings Jiu jitsu <kings.bjjoficial@gmail.com>
2025-08-06 14:56:03 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -25,9 +25,6 @@ const nextConfig = {
       }
     ],
   },
-  experimental: {
-    allowedDevOrigins: ["https://6000-firebase-studio-1750624255527.cluster-qhrn7lb3szcfcud6uanedbkjnm.cloudworkstations.dev"],
-  }
 };
 
 module.exports = nextConfig;
